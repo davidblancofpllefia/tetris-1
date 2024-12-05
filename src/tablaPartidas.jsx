7e338b3941a01fdbf801368573d38a213e79195d
Dani <Dani@gmail.com>
2024-12-05 13:43:11 +0100

    commit3historia2

--- src/tablaPartidas.jsx
+++ src/tablaPartidas.jsx
@@ -3,3 +3,10 @@ const [partidas, setPartidas] = useState([
     { id: 2, nombre: 'Partida 2', puntaje: 200 },
     { id: 3, nombre: 'Partida 3', puntaje: 150 },
   ]);
+
+  const ordenarPorColumna = (columna) => {
+    const partidasOrdenadas = [...partidas].sort((partidaA, partidaB) => 
+    partidaA[columna] === partidaB[columna] ? 0 : (partidaA[columna] < partidaB[columna] ? (ordenAscendente ? -1 : 1) : (ordenAscendente ? 1 : -1))
+  )}
+
+  setPartidas(partidasOrdenadas);
