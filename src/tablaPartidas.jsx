662889a306f27a753e2a062c4e978fe156bf6aa3
Dani <Dani@gmail.com>
2024-12-05 13:23:05 +0100

    commit2historia2

--- src/tablaPartidas.jsx
+++ src/tablaPartidas.jsx
@@ -1,1 +1,5 @@
-<div className=""></div>
+const [partidas, setPartidas] = useState([
+    { id: 1, nombre: 'Partida 1', puntaje: 100 },
+    { id: 2, nombre: 'Partida 2', puntaje: 200 },
+    { id: 3, nombre: 'Partida 3', puntaje: 150 },
+  ]);
